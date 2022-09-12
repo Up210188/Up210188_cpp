57b5c8b2dc6c6e3c006bcc728193131865c7ddf4
Up210188 <up210188@alumnos.upa.edu.mx>
2022-09-12 10:50:31 -0500

    primer programa

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,23 @@
+/*
+Date: 12/09/2022
+Author:Osvaldo Esparza Gutierrez 
+Email: up210188@alumnos.upa.edu.mx
+Description:Mi primer programa en C++
+*/
+
+//-----------PREPROCESOR DIRECTIVES-----------
+
+// input/output libary between screen and keyboard
+#include<iostream>
+
+//----------MAIN FUNCTION---------------
+int main(){
+
+//Prints on screen a text
+    std::cout<<"Hello World! This is my first program";
+
+//Return 0 means that the programm ends succesfully
+    return 0;
+}
+
+
