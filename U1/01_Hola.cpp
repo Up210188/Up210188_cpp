981c0af79683fe6ec633785f24e6a178ce94a3e8
Osvaldo <up210188@alumnos.upa.edu.mx>
2022-09-15 09:53:43 -0500

    actualizacion

--- U1/01_Hola.cpp
+++ U1/01_Hola.cpp
@@ -8,13 +8,13 @@ Description:Mi primer programa en C++
 //-----------PREPROCESOR DIRECTIVES-----------
 
 // input/output libary between screen and keyboard
-#include<iostream>
+#include <iostream>
 
 //----------MAIN FUNCTION---------------
 int main(){
 
 //Prints on screen a text
-    std::cout<<"Hello World! This is my first program";
+    std::cout<<"Hello World! This is my first program"; std::cout << "\n";
 
 //Return 0 means that the programm ends succesfully
     return 0;
