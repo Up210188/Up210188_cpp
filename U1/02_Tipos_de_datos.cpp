c7966bde674daf06d96fba0dacfd0c262fa0b587
Osvaldo <up210188@alumnos.upa.edu.mx>
2022-09-18 21:07:17 -0500

    actualizacion

---
+++ U1/02_Tipos_de_datos.cpp
@@ -0,0 +1,34 @@
+/*
+Date: 18/09/2022
+Author:Osvaldo Esparza Gutierrez 
+Email: up210188@alumnos.upa.edu.mx
+Description: Tipos de datos en C++
+*/
+
+//Libreria para manejo de entradas y salida de pantalla
+#include <iostream>
+
+//Uso del namespace para evitar el std::
+using namespace std;
+
+//Función principal de tipo entero
+int main()
+{
+
+    int entero = 6;
+    float flotante = 4.54638291876;
+    double grande = 4.54638291876;
+    char caracter = 5;
+    
+    cout << "Este programa muestra los tipos de datos. \n";
+    cout << "El número entero es: " << entero << endl;
+    cout << "El tamaño del entero es: " << sizeof(entero) << "bytes" << endl;
+    cout << "El número flotante es: " << flotante << endl;
+    cout << "El tamaño del flotante es: " << sizeof(flotante)<< "bytes" << endl;
+    cout << "El número double es: " << grande << endl;
+    cout << "El tamaño del double es: " << sizeof(grande)<< "bytes" << endl;
+    cout << "El caracter char es: " << caracter << endl;
+    cout << "El tamaño del char es: " << sizeof(caracter)<< "byte" << endl;
+    
+    return 0;
+}
