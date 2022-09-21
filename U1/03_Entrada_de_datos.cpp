0a968217b95020703f9e97da582d1fe9006dc64b
Osvaldo <up210188@alumnos.upa.edu.mx>
2022-09-21 10:40:44 -0500

    entrada

---
+++ U1/03_Entrada_de_datos.cpp
@@ -0,0 +1,54 @@
+/*
+Date: 21/09/2022
+Author:Osvaldo Esparza Gutierrez 
+Email: up210188@alumnos.upa.edu.mx
+Description: Entrada de datos en C++
+*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    int numero;
+    float flotante;
+    double grande;
+    char caracter;
+/*imprime un numero entero con iostream
+    cout<<"teclee un numero de tipo entero: "; cin>>numero;
+
+    cout<< "El numero guardado es: "<<numero<<endl;
+// imprime un numero flotante
+      cout<<"teclee un numero de tipo flotante: " ;cin>>flotante;
+      
+    cout<< "El numero guardado es: "<<flotante<<endl;
+// imprime un numero  double
+      cout<<"teclee un numero de tipo  double: "; cin>>grande;
+     
+    cout<< "El numero guardado es: "<<grande<<endl;
+
+    // imprime un digito en char
+      cout<<"teclee un caracter: "; cin>>caracter;
+     
+    cout<< "El caracter guardado es: "<<caracter<<endl;*/
+
+//------------------usando stdio.h-------------------------
+    cout<<endl;
+    cout<< "imprime con stdio.h \n";
+    
+//numero entero
+    printf("Dame un numero entero: ");
+    scanf("%d",&numero);
+    printf("El numero es: %d",numero);
+
+
+
+
+
+
+    return 0;
+
+
+}
+
+
