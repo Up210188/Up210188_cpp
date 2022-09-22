8579fa85a73dba99d27d93b5a8234136642840f5
Osvaldo <up210188@alumnos.upa.edu.mx>
2022-09-22 14:26:38 -0500

    actualizacion

---
+++ U2/01_.cpp
@@ -0,0 +1,32 @@
+/*
+Date: 22/09/2022
+Unidad:2
+Author:Osvaldo Esparza Gutierrez 
+Email: up210188@alumnos.upa.edu.mx
+Description:
+
+Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+
+Renta	Tipo impositivo
+Menos de $10,000	5%
+Entre $10,000 y $20,000	15%
+Entre $20,000 y $35,000	20%
+Entre $35,000 y $60,000	30%
+Más de $60,000	45%
+Escribir un programa que pregunte al usuario su renta anual y muestre por 
+pantalla el tipo impositivo que le corresponde.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
+
